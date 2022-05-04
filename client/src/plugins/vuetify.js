eafc0b247d5144d316b2117c780c0491f6c6a2a8
exolicious <48981822+exolicious@users.noreply.github.com>
2022-05-04 21:37:53 +0200

    change font-family and color theme

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -4,4 +4,14 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        light: true,
+        themes: {
+            light: {
+                primary: '#c21923',
+                secondary: '#555555',
+                accent: '#edb9af',
+            }
+        }
+    },
 });
